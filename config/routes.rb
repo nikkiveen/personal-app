9cb5ddaf8ad1dd6472fbb5244820cef226233eb6
Nikki Veen <nikkiveen@gmail.com>
2017-03-20 14:40:28 -0500

    install dotenv gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'pages#home'
   get '/home' => 'pages#home'
   get '/resume' => 'pages#resume'
+  get '/projects' => 'pages#projects'
+  get '/contact' => 'pages#contact'
 end
